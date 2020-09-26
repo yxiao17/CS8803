4bb446e2177c72f96066bdcc874ad1f76276b93f
Jonathan Chen <jonathan.as.chen@gmail.com>
2020-09-26 10:56:29 -0700

    recommended screen.. maybe

--- App.js
+++ App.js
@@ -20,11 +20,18 @@ import {
   Text,
   StatusBar,
   TouchableOpacity,
-    ImageBackground,
+  ImageBackground,
+  Dimensions,
+  FlatList,
+
 } from 'react-native';
 import * as theme from './theme';
+const { width, height } = Dimensions.get('window');
+import mocks from './RecommendeMock'
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 function HomeScreen({navigation}) {
+
   return (
       <View>
       <View style={{flexDirection: "row",
@@ -54,15 +61,82 @@ function HomeScreen({navigation}) {
           </TouchableOpacity>
           </View>
 
+          {/*recommended*/}
           <View>
 
               <Text style={{ fontSize: theme.sizes.font * 1.5, left:20 }}>Recommended</Text>
+              <View style={[styles.flex, styles.column, styles.recommended ]}>
+                  <View
+                      style={[
+                          styles.row,
+                          styles.recommendedHeader
+                      ]}
+                  >
+                      <Text style={{ fontSize: theme.sizes.font * 1.4 }}>Recommended</Text>
+                      <TouchableOpacity activeOpacity={0.5}>
+                          <Text style={{ color: theme.colors.caption }}>More</Text>
+                      </TouchableOpacity>
+                  </View>
+                  <View style={[styles.column, styles.recommendedList]}>
+                      <FlatList
+                          horizontal
+                          pagingEnabled
+                          scrollEnabled
+                          showsHorizontalScrollIndicator={false}
+                          scrollEventThrottle={16}
+                          snapToAlignment="center"
+                          style={[ styles.shadow, { overflow: 'visible' }]}
+                          data={mocks}
+                          keyExtractor={(item, index) => `${item.id}`}
+                          renderItem={({ item, index }) => {
+                            const destinations = mocks;
+                            const isLastItem = index === destinations.length - 1;
+                            console.log(destinations.length);
+                            return (
+                              <View style={[
+                                styles.flex, styles.column, styles.recommendation, styles.shadow,
+                                index === 0 ? { marginLeft: theme.sizes.margin } : null,
+                                isLastItem ? { marginRight: theme.sizes.margin / 2 } : null,
+                              ]}>
+                                <View style={[styles.flex, styles.recommendationHeader]}>
+                                  <Image style={[styles.recommendationImage]} source={{ uri: item.preview }} />
+                                  <View style={[ styles.flex, styles.row, styles.recommendationOptions ]}>
+                                    <Text style={styles.recommendationTemp}>
+                                      {item.temperature}℃
+                                    </Text>
+                                    <FontAwesome
+                                      name={item.saved ? 'bookmark' : 'bookmark-o'}
+                                      color={theme.colors.white}
+                                      size={theme.sizes.font * 1.25}
+                                    />
+                                  </View>
+                                </View>
+                                <View style={[styles.flex, styles.column, styles.shadow, { justifyContent: 'space-evenly', padding: theme.sizes.padding / 2 }]}>
+                                  <Text style={{ fontSize: theme.sizes.font * 1.25, fontWeight: '500', paddingBottom: theme.sizes.padding / 4.5, }}>{item.title}</Text>
+                                  <Text style={{ color: theme.colors.caption }}>{item.location}</Text>
+                                  <View style={[
+                                    styles.row,
+                                    { alignItems: 'center', justifyContent: 'space-between', marginTop: theme.sizes.margin }
+                                  ]}>
+                                    <Text style={{ color: theme.colors.active }}>
+                                      {item.rating}
+                                    </Text>
+                                  </View>
+                                </View>
+                              </View>
+                            )
+                          }
+                          }
+                      />
+                  </View>
+              </View>
           </View>
 
       </View>
 
   );
 }
+
 function Profile() {
         return (
             <View>
@@ -109,8 +183,17 @@ function App() {
 }
 
 const styles = StyleSheet.create({
-    avatar: {
+    flex: {
+        flex: 0,
+    },
+    column: {
+        flexDirection: 'column'
+    },
+    row: {
+        flexDirection: 'row'
+    },
 
+    avatar: {
         width: theme.sizes.padding*1.5,
         height: theme.sizes.padding*1.5,
         borderRadius: theme.sizes.padding / 2,
@@ -129,5 +212,45 @@ const styles = StyleSheet.create({
         flex: 1,
         resizeMode:'cover'
     },
+    recommended: {
+    },
+    recommendedHeader: {
+        justifyContent: 'space-between',
+        alignItems: 'flex-end',
+        paddingHorizontal: theme.sizes.padding,
+    },
+    recommendedList: {
+    },
+    recommendation: {
+        width: (width - (theme.sizes.padding * 2)) / 2,
+        marginHorizontal: 8,
+        backgroundColor: theme.colors.white,
+        overflow: 'hidden',
+        borderRadius: theme.sizes.radius,
+        marginVertical: theme.sizes.margin * 0.5,
+    },
+    recommendationHeader: {
+        overflow: 'hidden',
+        borderTopRightRadius: theme.sizes.radius,
+        borderTopLeftRadius: theme.sizes.radius,
+    },
+    recommendationOptions: {
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: theme.sizes.padding / 2,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+    },
+    recommendationTemp: {
+        fontSize: theme.sizes.font * 1.25,
+        color: theme.colors.white
+    },
+    recommendationImage: {
+        width: (width - (theme.sizes.padding * 2)) / 2,
+        height: (width - (theme.sizes.padding * 2)) / 2,
+    },
+
 })
 export default App;
